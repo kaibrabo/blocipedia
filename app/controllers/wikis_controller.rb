4564f89a4a91d4e774dc00057b6c4bce32b538d0
Kai Brabo <kaibrabo@gmail.com>
2017-08-16 15:14:37 -0700

    create wiki

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,17 +1,34 @@
 class WikisController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: [:index, :show]
+    skip_before_action :authenticate_user!, only: [:index, :show]
 
-  def index
-    @wikis = Wiki.all
-  end
+    def index
+        @wikis = Wiki.all
+    end
 
-  def show
-  end
+    def show
+        @wiki = Wiki.find(params[:id])
+    end
 
-  def new
-  end
+    def new
+        @wiki = Wiki.new
+    end
 
-  def edit
-  end
+    def create
+        @wiki = Wiki.new
+        @wiki.title = params[:wiki][:title]
+        @wiki.body = params[:wiki][:body]
+        @wiki.user = current_user
+
+        if @wiki.save!
+            flash[:notice] = "Wiki was Saved."
+            redirect_to @wiki
+        else
+            flash.now[:alert] = "There was an error saving your Wiki, Please try again."
+            render :new
+        end
+    end
+
+    def edit
+    end
 end
